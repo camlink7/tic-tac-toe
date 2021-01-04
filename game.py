b5981c05086e77eef2d993b21dcd8aa2ff6c33eb
camlink7 <camdenlink7@gmail.com>
2021-01-03 22:44:06 -0500

    added start and runtime methods

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import math
+import os
+from player import HumanPlayer, ComputerPlayer
 
 class TicTacToe:
     def __init__(self, x_player, o_player):
@@ -11,16 +13,27 @@ class TicTacToe:
     def print_board(self):
         #this simply prints the board with its current values
         
+        #clears the cmd or terminal
+        os.system('cls')
         #the first part is to only print -- if it is not the first row
         first = True
+        first2 = True
         for r in self.board:
             print('-- ' * 4) if not first else None
             first = False
             for c in r:
-                print('| '.join(c).join(' |'))
+                print('| '.join(c).join(' |')) if not first2 else None
+                first2 = False
 
-    def update_spot(self, r_spot, c_spot, ):
-        self.board[r_spot][c_spot] = "f"
+    def update_spot(self, r_spot, c_spot):
+        self.board[r_spot][c_spot] = self.current_player.letter
 
-game = TicTacToe()
+    def start(self):
+       self.current_player = self.x_player
+       self.runtime()
+
+    def runtime(self):
+        
+
+game = TicTacToe(HumanPlayer('x'), ComputerPlayer('o'))
 game.print_board()
--- player.py
+++ player.py
@@ -4,7 +4,6 @@ class Player:
     def __init__(self, letter):
         self.letter = letter
 
-
 class ComputerPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
