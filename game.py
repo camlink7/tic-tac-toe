c15d44ed4bd20b2abd995534ac41d9f909853777
camlink7 <camdenlink7@gmail.com>
2021-01-03 22:34:55 -0500

    created update_spot method

--- game.py
+++ game.py
@@ -1,11 +1,17 @@
 import math
 
 class TicTacToe:
-    def __init__(self):
+    def __init__(self, x_player, o_player):
         #declares a 2D Array with whitespaces
         self.board = [[' ' * 3], [' ' * 3], [' ' * 3]]
+        self.x_player = x_player
+        self.o_player = o_player
+        self.current_player = None
         
     def print_board(self):
+        #this simply prints the board with its current values
+        
+        #the first part is to only print -- if it is not the first row
         first = True
         for r in self.board:
             print('-- ' * 4) if not first else None
@@ -13,6 +19,8 @@ class TicTacToe:
             for c in r:
                 print('| '.join(c).join(' |'))
 
+    def update_spot(self, r_spot, c_spot, ):
+        self.board[r_spot][c_spot] = "f"
 
 game = TicTacToe()
 game.print_board()
