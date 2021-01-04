cac4527e3a4a87e0c36f0231f045d6448da9156c
camlink7 <camdenlink7@gmail.com>
2021-01-03 22:37:31 -0500

    removed game in main __init__

--- player.py
+++ player.py
@@ -1,16 +1,15 @@
 import math
 
 class Player:
-    def __init__(self, game, letter):
-        self.game = game
+    def __init__(self, letter):
         self.letter = letter
 
 
 class ComputerPlayer(Player):
-    def __init__(self, game, letter):
-        super().__init__(game, letter)
+    def __init__(self, letter):
+        super().__init__(letter)
 
 
 class HumanPlayer(Player):
-    def __init(self, game, letter):
-        super().__init__(game, letter)
+    def __init(self, letter):
+        super().__init__(letter)
