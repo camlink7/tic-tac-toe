3d2718e9c274652beba2848828376aca23e935a1
camlink7 <camdenlink7@gmail.com>
2021-01-03 22:12:35 -0500

    added ComputerPlayer class that feeds off Player

--- player.py
+++ player.py
@@ -1,4 +1,12 @@
+import math
+
 class Player:
     def __init__(self, game, letter):
         self.game = game
         self.letter = letter
+
+
+class ComputerPlayer(Player):
+    def __init__(self, game, letter):
+        super().__init__(game, letter)
+
