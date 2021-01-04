d227fc722f3d09dd35c873ecf87d8fcbab678701
camlink7 <camdenlink7@gmail.com>
2021-01-03 22:29:38 -0500

    added print_board method

--- game.py
+++ game.py
@@ -1,4 +1,18 @@
 import math
 
 class TicTacToe:
-    def __init__(self)
+    def __init__(self):
+        #declares a 2D Array with whitespaces
+        self.board = [[' ' * 3], [' ' * 3], [' ' * 3]]
+        
+    def print_board(self):
+        first = True
+        for r in self.board:
+            print('-- ' * 4) if not first else None
+            first = False
+            for c in r:
+                print('| '.join(c).join(' |'))
+
+
+game = TicTacToe()
+game.print_board()
