db8b3fb1c067c72f9651449e7cbecf0388981dea
camlink7 <camdenlink7@gmail.com>
2021-01-05 22:20:25 -0500

    added set_game() and get_move is random choice

--- player.py
+++ player.py
@@ -1,18 +1,23 @@
 import math
+import random
 
 class Player:
     def __init__(self, letter):
         self.letter = letter
+        self.game = None
 
     def get_move(self):
         return input()
+
+    def set_game(self, game):
+        self.game = game
 
 class ComputerPlayer(Player):
     def __init__(self, letter):
         super().__init__(letter)
     
     def get_move(self):
-        return('0,1')
+        return(random.choice(self.game.get_available_spots()))
     
 
 class HumanPlayer(Player):
